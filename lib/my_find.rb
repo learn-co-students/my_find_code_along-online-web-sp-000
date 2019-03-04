809aedc4b1adbb54f3815b7cd88eff5048b62148
Stuart Hahn <stuart.a.hahn@gmail.com>
2019-03-04 17:04:11 +0000

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,17 @@
 require 'pry'
 
 def my_find(collection)
-
+  if block_given?
+    i = 0
+    block_return_values = []
+    while i < collection.length
+      yield(collection[i])
+      i += 1
+    end
+    if block_return_values.detect != nil
+      block_return_values
+    else
+      false
+    end
+  end
 end
