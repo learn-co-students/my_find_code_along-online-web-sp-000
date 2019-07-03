44bc597fe13dc861d6fea5f761bd9c3e70dbeb40
Bernadette Davis <bsd231@nyu.edu>
2019-07-03 19:45:23 +0000

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,17 @@
 require 'pry'
 
 def my_find(collection)
+  found = nil
+  i = 0
+  while i < collection.length
+    if yield(collection[i])
+      found = collection[i]
+      break
+    end
 
+
+    i = i + 1
+  end
+
+  return found
 end
