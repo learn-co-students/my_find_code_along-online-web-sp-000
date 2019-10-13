028bda48c49eabbca1081d313331cd2c1123ceb4
github username <github email address>
2019-10-13 16:23:03 -0400

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,20 @@
 require 'pry'
+# def my_find(collection)
+#   i = 0
+#   while i < collection.length
+#     if yield(collection[i])
+#       return collection[i]
+#     end
+#     i = i + 1
+#   end
+# end
 
+# We can refactor this slightly by putting our if statement all on one line using a statement modifier:
+#///////////////////////////////////////////////////////////
 def my_find(collection)
-
+  i = 0
+  while i < collection.length
+    return collection[i] if yield(collection[i])
+    i += 1
+  end
 end
