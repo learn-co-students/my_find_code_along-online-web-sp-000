6039126ff2526fc288738bd9dc5be8b1e0ff50ad
Paul Marshall <pmarshal33@gmail.com>
2019-11-06 22:44:43 +0000

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,11 @@
 require 'pry'
 
-def my_find(collection)
-
-end
+ def my_find(array)
+   i = 0
+   while i < array.length 
+        if yield(array[i])
+          return array[i]
+        end
+        i = i + 1
+  end
+ end
