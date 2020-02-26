f36865ab286936cc2ecab8746e62c8d99f9ab35b
binding-archive-7591 <yarhumher@hotmail.com>
2020-02-26 19:37:28 +0000

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,36 @@
 require 'pry'
 
-def my_find(collection)
 
-end
+    def my_find(collection)
+              #   counter = 0
+              #   array = []
+              #  if collection == []
+              #   while counter < collection.length
+              #      if yield(collection[counter])
+              #       collection[counter]
+              #      end
+              #          counter += 1
+              #        end
+              #   else
+              #      while counter < collection.length
+              #      array << collection[counter]
+              #      counter += 1
+              #    end
+              #      return yield(array.find{|i| i.odd? })
+              # end
+              counter = 0
+
+      if (collection.length == 0)
+        return counter
+      end
+
+      while counter < collection.length
+        if yield(collection[counter])
+          return collection[counter]
+        end
+        counter = counter + 1
+      end
+
+      return nil
+
+    end
