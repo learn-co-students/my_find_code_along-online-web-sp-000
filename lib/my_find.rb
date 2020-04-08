c2cbd2c6ea7948f97e73d3b59afecd8fa8e929e1
Joseph Kao <josephtkao@gmail.com>
2020-04-08 19:22:09 +0000

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,15 @@
 require 'pry'
 
 def my_find(collection)
-
+  if block_given?
+    i = 0
+    while i < collection.length
+      return (collection[i]) if yield(collection[i])
+      i += 1
+    end
+  else
+    "No block given"
+  end
 end
+collection = (1..100).to_a
+print my_find(collection) {|i| i % 3 == 0 and i % 5 == 0 }
