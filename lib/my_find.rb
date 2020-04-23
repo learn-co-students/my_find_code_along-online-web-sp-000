98a4da1712d02d7a68245465a2698d3a5857ba25
Nicholas Robbins <nl7robbins@icloud.com>
2020-04-23 14:21:05 +0000

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,13 @@
 require 'pry'
 
-def my_find(collection)
+alg1 = {|element| puts element * element}
 
+def my_find(collection)
+  i = 0
+  while i < collection.length
+    yield(collection[i])
+    i += 1
+  end
 end
+
+my_find([1,2,3,4]) {|element| puts element * element}
