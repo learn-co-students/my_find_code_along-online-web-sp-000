cff2e8a72e1dcc8400dbf80965c6732779538ede
Nicholas Robbins <nl7robbins@icloud.com>
2020-04-23 14:24:12 +0000

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,13 +1,11 @@
 require 'pry'
 
-alg1 = {|element| puts element * element}
-
 def my_find(collection)
   i = 0
   while i < collection.length
-    yield(collection[i])
-    i += 1
+    if yield(collection[i])
+      return collection[i]
+    end
+    i = i + 1
   end
 end
-
-my_find([1,2,3,4]) {|element| puts element * element}
