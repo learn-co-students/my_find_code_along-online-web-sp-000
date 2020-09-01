30f08431bbda2299b5944976f3eca868644374e0
Steven Topazio <sat@northpointprinting.com>
2020-09-01 14:24:57 +0000

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -2,7 +2,8 @@ require 'pry'
 
 def my_find(collection)
   i = 0 
-  while i < collection.length return collection[i] if yield(collection[i])
+  while i < collection.length 
+  return collection[i] if yield(collection[i])
   i = i + 1 
 end
 
