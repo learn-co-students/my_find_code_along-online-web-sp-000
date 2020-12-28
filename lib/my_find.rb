f12c25fe625f1d811dd99793e0fc2a0c281850d8
Luis Martinez <luis_mmartinez@live.com>
2020-12-28 06:01:47 +0000

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,19 @@
 require 'pry'
 
 def my_find(collection)
+  i=0
+  block_return_values=[ ]
+  while i<collection.length
+  #  block_return_values << yield(collection[i])
+    if yield(collection[i])
+      return collection[i]
+    end
+    i+= 1
+  end
 
+  #if block_return_values.include?(true)
+#    collection[block_return_values.index(true)]
+#  else
+#    nil
+#  end
 end
